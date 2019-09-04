dd15c6a9e4618f897fc75da7a53b0c2dcf346b96
Michael Lustenberger <mic@inofix.ch>
2019-09-04 14:24:21 +0200

    Initialize Main
    Add the basic skeleton of the future code.

---
+++ main.py
@@ -0,0 +1,32 @@
+import click
+
+class Config(object):
+    def __init__(self):
+        self.macs = []
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+@click.group()
+@click.option('-d', '--device', 'macs', help='The MAC address of the BLE interface of the LifeBaseMeter to be scanned.', multiple=True)
+@pass_config
+def main(config, macs):
+    """Scan BLE devices for LifeBase parameters and send them to a MQTT broker."""
+    config.macs = macs
+
+@main.command()
+@pass_config
+def scan(config):
+    """Scan BLE devices for LifeBase parameters."""
+    for d in config.macs:
+        click.echo('Not scanning ' + d + ' yet..')
+
+@main.command()
+##TODO: multiple broker support?
+##TODO: certs, credentials, etc.
+@click.option('-h', '--hostname', 'brokerhost', default=None, help='The MQTT broker hostname to send the data to.')
+@click.option('-p', '--port', 'brokerport', default=None, help='The MQTT broker port to send the data to.')
+@pass_config
+def transport(config, brokerhost, brokerport):
+    """Scan the BLE devices and send the data to the MQTT broker."""
+    pass
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lifebase_ble_mqtt',
+    version='0.1',
+    py_modules=['lifebase_ble_mqtt'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        lifebase_ble_mqtt=main:main
+    ''',
+)
